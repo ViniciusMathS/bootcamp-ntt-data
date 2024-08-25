b16f2ab7bfdd453cea1203488c41691594243804
vinicius.math <viniciusmsms@gmail.com>
2024-08-25 11:27:45 -0300

    Adicionado os arquivos da aula sobre operadores em python

---
+++ aula_3/operadores_associacao.py
@@ -0,0 +1,9 @@
+frutas = ["kiwi", "uva", "limão"]
+
+print("laranja" in frutas)
+print("laranja" not in frutas) #verifica apresença de um item na lista, este operador é case sensitive para procurar strings
+
+curso = "Curso de Python"
+
+print("Python" in curso)
+print("python" in curso)

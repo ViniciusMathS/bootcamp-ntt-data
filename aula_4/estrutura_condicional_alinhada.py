be992a61db7b6430bd1f56b4d1cff70b4d608936
vinicius.math <viniciusmsms@gmail.com>
2024-08-25 21:29:51 -0300

    Adicionado arquivos da aula sobre estruturas de repetição for e while

--- aula_4/estrutura_condicional_alinhada.py
+++ aula_4/estrutura_condicional_alinhada.py
@@ -1,8 +1,9 @@
 conta_normal = True
-conta_universitaria = False
+conta_universitaria = True
+conta_especial = True
 
 saldo = 2000
-saque = 500
+saque = 2400
 cheque_especial = 450
 
 if conta_normal:
@@ -10,8 +11,14 @@ if conta_normal:
         print("Saque realizado.")
     elif saque <= (saldo + cheque_especial):
         print("Saque realizado com uso do cheque especial.")
+    else:
+        print("Saldo insuficiente.")
 elif conta_universitaria:
     if saldo >= saque:
         print("Saque realizado.")
     else:
-        print("Saldo insuficiente.")    
+        print("Saldo insuficiente.")
+elif conta_especial:
+    print("Conta especial selecionada.")
+else:
+    print("Tipo de conta invalida.")

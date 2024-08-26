ea4a77dd418d6c622ba46c86b9651477db9b2b0f
vinicius.math <viniciusmsms@gmail.com>
2024-08-25 23:16:42 -0300

    Adicionado arquivos sobre manipulação de strings em python, formatação, split, interpolação e strings de multiplas linhas

---
+++ aula_5/string_1.py
@@ -0,0 +1,18 @@
+nome = "vIniCius"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  ola mundo!   "
+
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Python"
+
+print("####" + menu + "####")
+print(menu.center(14, "*"))
+print("_".join(menu))
+print(menu)

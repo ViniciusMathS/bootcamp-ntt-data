aaf2514eac2f11bfda1f4772af7db09b289d986a
vinicius.math <viniciusmsms@gmail.com>
2024-08-30 15:26:03 -0300

    Adicionado os arquivos sobre tuplas

---
+++ aula_7/08_metodos_tupla.py
@@ -0,0 +1,14 @@
+# tuplas não possuem métodos de deleção e atualização de valores
+
+print("\ncount")
+cores = ("verde", "azul", "vermelho", "verde",)
+print(cores.count("verde"))
+print(cores.count("azul"))
+print(cores.count("vermelho"))
+
+print("\nindex")
+print(cores.index("azul"))
+print(cores.index("verde"))
+
+print("\nlen")
+print(len(cores))
